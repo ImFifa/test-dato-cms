2ce6e95a559f4aad07fc3ba58f745e99846ade40
Filip Urban - desktop <info@filipurban.cz>
2020-11-11 18:43:15 +0100

    adding contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ContactPage = () => (
+  <div>
+    <h1>Contact</h1>
+    <p>Hey man! Welcome to our contact page! You should see a contact form apear here very very soon. We're currently working on it!</p>
+  </div>
+)
+
+export default ContactPage
