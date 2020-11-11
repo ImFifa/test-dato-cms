8c5ecd9788ca04fbe036e31ba87a8098872ef8c0
Filip Urban - desktop <info@filipurban.cz>
2020-11-11 18:49:07 +0100

    layout added to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Layout from "../components/layout"
 
 const ContactPage = () => (
-  <div>
-    <h1>Contact</h1>
-    <p>Hey man! Welcome to our contact page! You should see a contact form apear here very very soon. We're currently working on it!</p>
-  </div>
+    <Layout>
+    <article className="sheet">
+        <div>
+            <h1>Contact</h1>
+            <p>Hey man! Welcome to our contact page! You should see a contact form apear here very very soon. We're currently working on it!</p>
+        </div>
+    </article>
+  </Layout>
 )
 
 export default ContactPage
